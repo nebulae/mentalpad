75524863a00875d4cd65e58a10d70b4339496037
nebulae <nebulae@gmail.com>
2015-10-21 15:08:16 -0700

    initial commit - added template, beginning of resume, background images, skeleton of app engine project

---
+++ main.py
@@ -0,0 +1,15 @@
+import webapp2
+import urllib2
+import json
+import logging
+import handlers
+
+class MainHandler(handlers.BaseTemplateHandler):
+    def get(self):
+        self.render_template("main.html", {})
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+    # webapp2.Route('/game/<gameid>', handler=GameHandler),
+    # webapp2.Route('/grid/<subreddit>', handler=GridHandler)
+], debug=True)
